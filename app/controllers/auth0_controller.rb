e8390fcd9b5aa40592b247ce665f7792e2a2c7d7
Ticean Bennett <ticean@gmail.com>
2015-02-25 13:35:00 -0800

    SSO endpoint for autologin and deep link

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -13,4 +13,33 @@ class Auth0Controller < ApplicationController
     Rails.logger.info("Login failure. #{request.params['message']}")
     @error_msg = request.params['message']
   end
+
+  # Performs a simple check for a user session. If the user is logged in then
+  # redirect to the target URL specified in the `targetUrl` querystring
+  # parameter.
+  #
+  # If the user is not logged in, redirect to initiate the authorizion flow on
+  # the identity provider. If the user has a session on the SSO then she will
+  # be automatically logged in.
+  #
+  # NOTE: The redirect target URL is provided via a querystring param. This
+  # method is not secure, as users could be redirected to non-secure third-party
+  # URLs. The param should be checked that it is a link within this application.
+  def sso
+    target_url = params['targetUrl']
+    redirect_link = target_url.present? ? target_url : '/'
+    if session[:userinfo].present?
+      # User is logged in.
+      redirect_to redirect_link
+    else
+      # User is not logged in.
+      auth_url = "https://#{ENV['AUTH0_DOMAIN']}/authorize?" \
+                 "response_type=code" \
+                 "&scope=openid" \
+                 "&client_id=#{ENV['AUTH0_CLIENT_ID']}" \
+                 "&redirect_uri=#{ENV['AUTH0_CALLBACK_URL']}" \
+                 "&state=#{redirect_link}"
+      redirect_to auth_url
+    end
+  end
 end
