89727245d386b1d1bd507eb8b60d3e2fab2c14ca
Ticean Bennett <ticean@gmail.com>
2015-03-07 15:18:21 -0800

    Title based on ENV app name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def render_title
+    prefix = ENV['APPLICATION_NAME']
+    if defined?(@title)
+      "#{prefix }: #{title}"
+    else
+      prefix
+    end
+  end
 end
